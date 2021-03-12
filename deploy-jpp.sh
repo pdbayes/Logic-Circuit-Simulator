406f531700c7fbd4a03a9d362bd4bb8e9c50bcce
Jean-Philippe Pellet <jppellet@gmail.com>
2021-03-12 11:29:41 +0100

    Helper scripts

---
+++ deploy-jpp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+src_folder=$(dirname "$0")
+
+host="ua898908.serversignin.com"
+port="8228"
+username="ua898908"
+target_path="/home/ua898908/public_html/jp.pellet.name/hep/logiga"
+
+rsync -avzcP -e "ssh -p $port" "$src_folder" "$username@$host:$target_path"
