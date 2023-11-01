86677c267f74bb7a1557f5ce655a8016cfae3c9c
Jean-Philippe Pellet <jppellet@gmail.com>
2023-11-01 10:21:17 +0100

    Correct typo in clock context menu

--- simulator/src/components/Clock.ts
+++ simulator/src/components/Clock.ts
@@ -174,7 +174,7 @@ export class Clock extends InputBase<ClockRepr> {
             [2000, "2 s (0.5 Hz)"],
             [4000, "4 s (0.25 Hz)"],
             [8000, "8 s (0.125 Hz)"],
-            [16000, "8 s (0.0625 Hz)"],
+            [16000, "16 s (0.0625 Hz)"],
         ]
 
         const makeItemSetPeriod = (data: [number, string]) => {
